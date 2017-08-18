26f5ed315c26b29bb9e78ca862c78699eff511cf
AmanMinhas <amandeepSinghMinhas@gmail.com>
2017-08-18 23:05:47 +0530

    Removed import for deleted file

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import AutoSuggestWrapper from './AutoSuggestWrapper';
 import * as ordersActions from '../mods/orders/action.js';
-import '../style/react-autosuggest.css';
+// import '../style/react-autosuggest.css';
 // import socketService from '../mods/socket/SocketService';
 
 class Orders extends Component {
